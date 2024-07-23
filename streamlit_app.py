2b06de266922758b0d0c7074c8a31a97fd9bcb99
Greatnessbio <154548997+Greatnessbio@users.noreply.github.com>
2024-07-23 12:15:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def fetch_google_search_results(query: str, num_results: int):
     "hl": "en",
     "gl": "us",
     "google_domain": "google.com",
-    "location": "New York,New York,United States",
+    "location": "Austin,Texas,United States",
     "device": "desktop",
     "include_ads": "true",
 }
